c998f11fec63ee6ed38508e9d93880a719ecc43b
Neil Manimtim <neil@Neils-MacBook-Pro.local>
2018-05-06 21:30:06 -0700

    finsihed

--- cookie-clicker-lab-7-2/src/main/java/edu/csula/storage/servlet/UsersDAOImpl.java
+++ cookie-clicker-lab-7-2/src/main/java/edu/csula/storage/servlet/UsersDAOImpl.java
@@ -22,19 +22,29 @@ public class UsersDAOImpl implements UsersDAO {
 
 	@Override
 	public boolean authenticate(String username, String password) {
-		// TODO: check if username/password combination is valid and store the
-		//       username/password into the session
+		if (username.equals("admin") && password.equals("cs3220password")) {
+			User user = new User(0, username, password);
+			context.setAttribute(CONTEXT_NAME, user);
+
+			return true;
+		}
+
 		return false;
 	}
 
 	@Override
 	public Optional<User> getAuthenticatedUser() {
-		// TODO: return the authenticated user if there is any
-		return Optional.empty();
+		Object data = context.getAttribute(CONTEXT_NAME);
+
+		if (data == null) {
+			return Optional.empty();
+		}
+		
+		return Optional.of((User) data );
 	}
 
 	@Override
 	public void logout() {
-		// TOOD: log user out using `invalidate`
+		context.invalidate();
 	}
 }
--- cookie-clicker-lab-7-2/src/main/java/edu/csula/web/AdminEventsServlet.java
+++ cookie-clicker-lab-7-2/src/main/java/edu/csula/web/AdminEventsServlet.java
@@ -2,7 +2,8 @@ package edu.csula.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,26 +11,76 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import edu.csula.storage.servlet.EventsDAOImpl;
 import edu.csula.storage.EventsDAO;
+import edu.csula.storage.mysql.*;
 import edu.csula.models.Event;
 
 @WebServlet("/admin/events")
 public class AdminEventsServlet extends HttpServlet {
 	@Override
 	public void doGet( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		// TODO: render the events page HTML
-		EventsDAO dao = new EventsDAOImpl(getServletContext());
-		Collection<Event> events = dao.getAll();
-		System.out.println(events);
-		out.println("<h1>Hello events servlet!</h1>");
+		EventsDAO dao = new EventsDAOImpl(new Database());
+		List<Event> events = (ArrayList<Event>) dao.getAll();
+		
+		request.setAttribute("events", events);
+
+		if (request.getParameter("id") != null) {
+			int index = retrieveIndex(Integer.parseInt(request.getParameter("id")));
+			request.setAttribute("index", index);
+		}
+
+		if (request.getParameter("deleteId") != null) {
+			int id = Integer.parseInt(request.getParameter("deleteId"));
+			dao.remove(id);
+			response.sendRedirect("events");
+
+			return;
+		}
+
+		request.getRequestDispatcher("/WEB-INF/admin-events.jsp").forward(request, response);
 	}
 
 
 	@Override
 	public void doPost( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO: handle upsert transaction
+		EventsDAO dao = new EventsDAOImpl(new Database());
+		List<Event> events = (ArrayList<Event>) dao.getAll();
+
+		String name = request.getParameter("eventName");
+		String description = request.getParameter("eventDescription");
+		String triggerAt = request.getParameter("eventTriggerAt");	
+
+		String idStr = request.getParameter("id");
+
+		if (idStr != null) {
+			int id = Integer.parseInt(idStr);
+
+			Event event = events.get(retrieveIndex(id));
+			event.setName(name);
+			event.setDescription(description);
+			event.setTriggerAt(Integer.parseInt(triggerAt));
+			dao.set(id, event);
+		}
+
+		else {
+			Event event = new Event(dao.getAll().size(), name, description, Integer.parseInt(triggerAt));
+			dao.add(event);
+		}
+
+		response.sendRedirect("events");
+	}
+
+	public int retrieveIndex(int id) {
+		EventsDAO dao = new EventsDAOImpl(new Database());
+		ArrayList<Event> events = (ArrayList<Event>) dao.getAll();
+
+		for (int i = 0 ; i < events.size() ; i++) {
+			if (id == events.get(i).getId()) {
+				return i;
+			}
+		}
+
+		return -1;
+
 	}
 }
