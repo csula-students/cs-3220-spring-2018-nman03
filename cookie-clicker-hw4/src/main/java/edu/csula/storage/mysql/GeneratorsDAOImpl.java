ebe966f13f43d70d2592d70322b1f7208089cd92
Neil Manimtim <neil@Neils-MacBook-Pro.local>
2018-05-08 00:00:31 -0700

    passed tests

--- cookie-clicker-hw4/src/main/java/edu/csula/storage/mysql/GeneratorsDAOImpl.java
+++ cookie-clicker-hw4/src/main/java/edu/csula/storage/mysql/GeneratorsDAOImpl.java
@@ -3,6 +3,7 @@ package edu.csula.storage.mysql;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.sql.*;
 
 import edu.csula.storage.GeneratorsDAO;
 import edu.csula.storage.Database;
@@ -13,11 +14,11 @@ public class GeneratorsDAOImpl implements GeneratorsDAO {
 
 	// TODO: fill the Strings with the SQL queries as "prepated statements" and
 	//       use these queries variable accordingly in the method below
-	protected static final String getAllQuery = "";
-	protected static final String getByIdQuery = "";
-	protected static final String setQuery = "";
-	protected static final String addQuery = "";
-	protected static final String removeQuery = "";
+	protected static final String getAllQuery = "SELECT * FROM generators;";
+	protected static final String getByIdQuery = "SELECT * FROM generators WHERE id = ?";
+	protected static final String setQuery = "UPDATE generators SET name = ?, description = ?, rate =  ?, base_cost = ?, unlock_at = ? WHERE id = ?;";
+	protected static final String addQuery = "INSERT INTO generators (name, description, rate, base_cost, unlock_at) VALUES (?, ?, ?, ?, ?);";
+	protected static final String removeQuery = "DELETE FROM generators WHERE id = ?;";
 
 	public GeneratorsDAOImpl(Database context) {
 		this.context = context;
@@ -25,28 +26,72 @@ public class GeneratorsDAOImpl implements GeneratorsDAO {
 
 	@Override
 	public List<Generator> getAll() {
-		// TODO: get all generators from jdbc
-		return new ArrayList<>();
+		List<Generator> result = new ArrayList<>();
+		try (Connection c = context.getConnection(); Statement stmt = c.createStatement()) {
+			ResultSet rs = stmt.executeQuery(getAllQuery);
+			while (rs.next()) {
+				Generator generator = new Generator(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5), rs.getInt(6));
+			
+				result.add(generator);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return new ArrayList<>();
+		}
+		return result;
 	}
 
 	@Override
 	public Optional<Generator> getById(int id) {
-		// TODO: get specific generator by id
-		return Optional.empty();
+		Generator result;
+		try (Connection c = context.getConnection(); PreparedStatement stmt = c.prepareStatement(getByIdQuery)) {
+			stmt.setInt(1, id);
+			ResultSet rs = stmt.executeQuery();
+			result = new Generator(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5), rs.getInt(6));
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return Optional.empty();
+		}
+		return Optional.of(result);
 	}
 
 	@Override
 	public void set(int id, Generator generator) {
-		// TODO: update specific generator by id
+		try (Connection c = context.getConnection(); PreparedStatement stmt = c.prepareStatement(setQuery)) {
+			stmt.setString(1, generator.getName());
+			stmt.setString(2, generator.getDescription());
+			stmt.setInt(3, generator.getRate());
+			stmt.setInt(4, generator.getBaseCost());
+			stmt.setInt(5, generator.getUnlockAt());
+			stmt.setInt(6, id);
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
 	}
 
 	@Override
 	public void add(Generator generator) {
-		// TODO: implement jdbc logic to add a new generator
+		try (Connection c = context.getConnection(); PreparedStatement stmt = c.prepareStatement(addQuery)) {
+			stmt.setString(1, generator.getName());
+			stmt.setString(2, generator.getDescription());
+			stmt.setInt(3, generator.getRate());
+			stmt.setInt(4, generator.getBaseCost());
+			stmt.setInt(5, generator.getUnlockAt());
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
 	}
 
 	@Override
 	public void remove(int id) {
-		// TODO: implement jdbc logic to remove generator by id
+		try (Connection c = context.getConnection(); PreparedStatement stmt = c.prepareStatement(removeQuery)) {
+			stmt.setInt(1, id);
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
 	}
 }
--- cookie-clicker-hw4/src/main/java/edu/csula/web/AdminGeneratorsServlet.java
+++ cookie-clicker-hw4/src/main/java/edu/csula/web/AdminGeneratorsServlet.java
@@ -8,19 +8,84 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import edu.csula.storage.GeneratorsDAO;
+import edu.csula.storage.mysql.*;
+import edu.csula.storage.mysql.*;
+import edu.csula.models.Generator;
+
 @WebServlet("/admin/generators")
 public class AdminGeneratorsServlet extends HttpServlet {
 	@Override
 	public void doGet( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		// TODO: render the generators page HTML
-		out.println("<h1>Hello generators servlet!</h1>");
+		GeneratorsDAO dao = new GeneratorsDAOImpl(new Database());
+		List<Generator> generators = (ArrayList<Generator>) dao.getAll();
+		
+		request.setAttribute("generators", generators);
+
+		if (request.getParameter("id") != null) {
+			int index = retrieveIndex(Integer.parseInt(request.getParameter("id")));
+			request.setAttribute("index", index);
+		}
+
+		if (request.getParameter("deleteId") != null) {
+			int id = Integer.parseInt(request.getParameter("deleteId"));
+			dao.remove(id);
+			response.sendRedirect("generators");
+
+			return;
+		}
+
+		request.getRequestDispatcher("/WEB-INF/admin-generators.jsp").forward(request, response);
 	}
 
 
 	@Override
 	public void doPost( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO: handle upsert transaction
+		GeneratorsDAO dao = new GeneratorsDAOImpl(new Database());
+		List<Generator> generators = (ArrayList<Generator>) dao.getAll();
+
+		String name = request.getParameter("generatorName");
+		String description = request.getParameter("generatorDescription");
+		String rate = request.getParameter("generatorRate");
+		String baseCost = request.getParameter("generatorBaseCost");
+		String unlockAt = request.getParameter("generatorUnlockAt");	
+
+		String idStr = request.getParameter("id");
+
+		if (idStr != null) {
+			int id = Integer.parseInt(idStr);
+
+			Generator generator = dao.getById(id).get();
+			generator.setName(name);
+			generator.setDescription(description);
+			generator.setRate(Integer.parseInt(rate));
+			generator.setBaseCost(Integer.parseInt(baseCost));
+			generator.setUnlockAt(Integer.parseInt(unlockAt));
+			dao.set(id, generator);
+		}
+
+		else {
+			Generator generator = new Generator(dao.getAll().size(), name, description, Integer.parseInt(rate), 
+				Integer.parseInt(baseCost), Integer.parseInt(unlockAt));
+			dao.add(generator);
+		}
+
+		response.sendRedirect("generators");
+	}
+
+	public int retrieveIndex(int id) {
+		GeneratorsDAO dao = new GeneratorsDAOImpl(new Database());
+		ArrayList<Generator> generators = (ArrayList<Generator>) dao.getAll();
+
+		for (int i = 0 ; i < generators.size() ; i++) {
+			if (id == generators.get(i).getId()) {
+				return i;
+			}
+		}
+
+		return -1;
 	}
 }
